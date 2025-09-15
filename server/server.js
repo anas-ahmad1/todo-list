162b2f5dc34bad354cf2f5dadc215de5d6eb729f
Anas Ahmad <anasahmadcs7@gmail.com>
2025-09-16 03:26:19 +0500

    implement user registration route and controller

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 
+app.use('/auth', require('./src/routes/authRoutes'))
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please add all fields");
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({message: "User already exists"});
+      throw new Error("User already exists");
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  });
+
+
+
+
+  const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+  };
+  
+  module.exports = { registerUser };
