1a85c57d9b7918dda115c89c22731c265f66ef2b
Anas Ahmad <anasahmadcs7@gmail.com>
2025-09-18 05:16:29 +0500

    implement logout and jwt blacklist functionality

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require("../models/User");
+const TokenBlacklist = require("../models/TokenBlacklist");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token
@@ -12,6 +13,13 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       // Get token from header
       token = req.headers.authorization.split(' ')[1]
+
+      // Check if token is blacklisted or not
+      const blacklisted = await TokenBlacklist.findOne({ token });
+      if (blacklisted) {
+        res.status(401);
+        throw new Error("Not authorized, token is blacklisted");
+      }
 
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUser } = require("../controllers/authController");
+const { registerUser, loginUser, getUser, logoutUser } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
@@ -8,5 +8,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.get("/me", protect, getUser);
+
+router.post("/logout", logoutUser);
 
 module.exports = router;
